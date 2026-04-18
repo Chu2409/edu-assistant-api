c79a34f0cec578a7d77f0a2c7ae1e95e1b80ca63
scaminom <sebastian.camino@proconty.com>
2026-04-17 23:57:35 -0500

    refactor(videos): delete dead ContentAgent interface file

--- src/features/videos/ai/interfaces/content-agent.interface.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-import { BlockType } from 'src/core/database/generated/client'
-import { TaskName } from '../config/task-name.type'
-
-export interface ContentAgent {
-  blockType: BlockType
-  taskName: TaskName
-  schema: z.ZodType
-}
